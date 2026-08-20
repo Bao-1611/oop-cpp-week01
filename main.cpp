26bf2a2de2014046ed237ee922062c0e2e59a8e0
Nguyen Chi Bao <ncbslav@gmail.com>
2026-08-20 23:13:19 +0700

    Even more Minor Improvements

--- main.cpp
+++ main.cpp
@@ -4,24 +4,25 @@ int main()
 {
     // Header section with separators
     std::cout << "========================\n";
-    std::cout << "    MY FIRST C++ APP    \n";
+    std::cout << "         PROFILE        \n";
     std::cout << "------------------------\n";
 
     // Blank line for clean spacing
     std::cout << std::endl;
 
     // Content section
-    std::cout << "Name: Bao\n";
-    std::cout << "Course: OOP with C++\n";
-    std::cout << "Im ready to learn C++!\n";
-    
+    std::cout << "  [Name]         : Bao" << std::endl;
+    std::cout << "  [Major]        : Automotive Technology and Engineering" << std::endl;
+    std::cout << "  [Favorite Car] : Mercedes-Benz AMG CLS63s" << std::endl;
+    std::cout << "  [Setup]        : AMD Ryzen 7 4800H | GTX 1650" << std::endl;
+
 
     std::cout << std::endl;
 
     // Footer section
-    std::cout << "---------------------------" << std::endl;
-    std::cout << "    Execution complete" << std::endl;
-    std::cout << "===========================" << std::endl;
+    std::cout << "----------------------------------------------------------" << std::endl;
+    std::cout << "    IF THIS SHOWS UP ON GITHUB THEN I DID IT CORRECTLY    " << std::endl;
+    std::cout << "==========================================================" << std::endl;
 
     return 0;
 }
