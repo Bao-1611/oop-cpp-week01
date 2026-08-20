71057f2960069cfa238a60a052ea05d82ea46d81
Nguyen Chi Bao <ncbslav@gmail.com>
2026-08-20 22:47:29 +0700

    Add formatted output

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream> 
 
-int main() {
-    std::cout << "Hello, My name is Bao!" << std::endl;
-    std::cout << "Welcome to OOP with C++.." << std::endl;
+int main() 
+{
+    std::cout << "========================\n";
+    std::cout << "    MY FIRST C++ APP    \n";
+    std::cout << "========================\n";
+    std::cout << "Name: Bao\n";
+    std::cout << "Course: OOP with C++\n";
+    std::cout << "Im ready to learn C++!\n";
     return 0;
 }
